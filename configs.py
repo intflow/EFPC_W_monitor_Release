ece1e664b91def55f006ac5794ef8943ea63c468
intflow <inchuuul.ryu@gmail.com>
2022-12-27 11:20:40 +0900

    container name changed

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ docker_image_tag_header_list = ["dev", "res"] # res 우선
 docker_image_tag_header = "None" # Don't Touch!! 수정하지 말고 놔두기!! 자동으로 잡음.
 edgefarm_config_path = "/edgefarm_config/edgefarm_config.json"
 edgefarm_port_info_path = "/edgefarm_config/port_info.txt"
-container_name = "efhall_test"
+container_name = "edgefarm_docker"
 commit_container_name = "for_commit"
 
 key_match_dict = {
