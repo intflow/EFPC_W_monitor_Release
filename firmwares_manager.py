587bc3e3b6dd8dcd6ba360adc94d86eb8d934e85
intflow <inchuuul.ryu@gmail.com>
2023-01-09 11:34:12 +0900

    update_time added

--- firmwares_manager.py
+++ firmwares_manager.py
@@ -4,6 +4,8 @@ import configs
 from utils import *
 import shutil
 import subprocess
+import json
+import datetime as dt
 
 git_pull_done = False
 c_dir = os.path.dirname(os.path.abspath(__file__))
@@ -37,8 +39,30 @@ def git_pull():
     now_dt = dt.datetime.now()
     # print(now_dt)
     
-    # 11시 50분에 pull 받기
-    if now_dt.hour == 23 and now_dt.minute >= 50:
+    edgefarm_config = {}
+
+    update_time_str = ""
+
+    # file read
+    with open(configs.edgefarm_config_path, "r") as edgefarm_config_file:
+        edgefarm_config = json.load(edgefarm_config_file)
+
+    if "update_time" in edgefarm_config:
+        update_time_str = edgefarm_config["update_time"]
+    # else:
+
+    if len(update_time_str) > 0:
+        update_time_slice = update_time_str.split(":")
+        if len(update_time_slice) == 3:
+            u_hour, u_min, u_sec = list(map(int,update_time_slice))
+        else:
+            print("Invalid data type : \"update_time\"")
+    else:
+        u_hour, u_min, u_sec = [23, 50, 0]
+          
+    
+    # pull 받기
+    if now_dt.hour == u_hour and now_dt.minute >= u_min:
     # if now_dt.hour == 16 and now_dt.minute >= 38:
         print("\n  git pull from remote repository")
         try:
