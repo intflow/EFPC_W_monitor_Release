a14b3117d909b1231c32ef42197fba2f683217d6
inchulryu <inchuuul.ryu@gmail.com>
2023-01-18 17:07:26 +0900

    efpc double fix

--- for_supervisor.py
+++ for_supervisor.py
@@ -95,10 +95,6 @@ if __name__ == "__main__":
     firmwares_manager.copy_firmwares()
     
     device_install()
-    
-    if is_process_running("efpc_box") == False:
-        efpc_box_process = multiprocessing.Process(target=run_blackBox)
-        efpc_box_process.start()
 
     # 폴더 자동삭제를 위한 설정
     f = open("/edgefarm_config/Smart_Record.txt","rt")
